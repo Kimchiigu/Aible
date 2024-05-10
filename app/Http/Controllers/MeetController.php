827805e48353a79bdbb7511fcd6fb3e41adeee52
F4E12 <76392444+F4E12@users.noreply.github.com>
2024-05-10 21:33:11 +0700

    21.23 10/5/2024

--- app/Http/Controllers/MeetController.php
+++ app/Http/Controllers/MeetController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class MeetController extends Controller
 {
@@ -10,8 +12,12 @@ class MeetController extends Controller
         $basePath = base_path();
 
         $pythonScriptPath = $basePath . '/resources/python/camera.py';
+        $process = new Process(['python', $pythonScriptPath]);
+        $process->run();
 
-        shell_exec("python " . escapeshellarg($pythonScriptPath));
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
         return view('meet', [
             'title' => "Register"
         ]);
