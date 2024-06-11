77ec675543254073dd7564f090d245abbf56349c
Myanee <136609271+Myaneeeee@users.noreply.github.com>
2024-06-11 19:52:10 +0700

    benerin material seeder

--- database/seeders/MaterialSeeder.php
+++ database/seeders/MaterialSeeder.php
@@ -17,13 +17,23 @@ class MaterialSeeder extends Seeder
      */
     public function run()
     {
-        $title = Str::random(8);
-        DB::table('materials')->insert([
-            'title' => $title,
-            'slug' => Str::slug(strtolower($title), '_'),
-            'excerpt' => Lorem::ipsumWords(15),
-            'body' => Lorem::ipsumParagraph(5),
-            'src' => "assets/images/imgnotfound.jpg"
-        ]);
+        $materials = [
+            [
+                'title' => 'Introduction to Computer Programming',
+                'slug' => "introductiontocomputerprogramming",
+                'excerpt' => 'Covers basic programming concepts and languages such as Python, Java, or C++. Emphasizes problem-solving and logical thinking skills.',
+                'body' => 'This course introduces learners to the fundamental principles of computer programming, focusing on developing problem-solving skills through coding. It includes modules on syntax, control structures, data types, and algorithms. Students will work on hands-on projects to practice writing and debugging code. By the end of the course, participants will be able to create simple programs and understand the basics of how software is developed.',
+                'src' => 'assets/images/programmingtraining.webp',
+            ],
+            [
+                'title' => 'Graphic Design Fundamentals',
+                'slug' => 'graphicdesignfundamentals',
+                'excerpt' => 'Teaches the principles of graphic design, including typography, color theory, and layout, using software like Adobe Photoshop and Illustrator.',
+                'body' => 'This training material covers the core concepts of graphic design, helping learners develop a strong foundation in visual communication. Topics include understanding color theory, selecting appropriate fonts, and creating balanced layouts. The course also includes practical exercises using industry-standard software such as Adobe Photoshop and Illustrator, allowing students to apply their knowledge in real-world scenarios and create professional-quality designs.',
+                'src' => 'assets/images/graphicdesigntraining.jpg',
+            ]
+        ];
+
+        DB::table('materials')->insert($materials);
     }
 }
