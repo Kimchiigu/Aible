fa4543c86b4b6813de4012aa79ffb8bdf446958f
Christopher Hardy Gunawan <christopher.hygunawan@gmail.com>
2024-06-12 08:59:55 +0700

    fixing button at task manager

--- resources/views/taskmanager.blade.php
+++ resources/views/taskmanager.blade.php
@@ -93,7 +93,9 @@
             <button class="remove-task-button focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Remove Task</button>
         </div>
     </div>
-    <button id="add-task-button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-0 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add More Task</button>
-    <button id="task-maker" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-0 dark:focus:ring-yellow-900">Help Me</button>
+    <div class="flex flex-row gap-3">
+        <button id="add-task-button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-0 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add More Task</button>
+        <button id="task-maker" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-0 dark:focus:ring-yellow-900">Help Me</button>
+    </div>
     <script src="{{ asset('javascript/taskmanager.js') }}"></script>
 @endsection
